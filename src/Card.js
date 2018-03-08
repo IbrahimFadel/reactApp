21ab3ca1855e4f5db89b731f45d382152c6e3a39
IbrahimFadel <ibrahim.fadel@ucc.on.ca>
2018-03-08 12:43:34 -0500

    added first component
    First Component

---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class Card extends Component {
+    render() {
+        return(
+            <div styleName="tc">
+                <h1>Title</h1>
+                <p>This is a sample component made with ReactJs</p>
+            </div>
+        );
+    }
+}
+
+export default Card;
